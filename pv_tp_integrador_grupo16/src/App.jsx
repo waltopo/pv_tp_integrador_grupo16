3ca2a5b2504ab3d3d98b480cc3a85bfc2aab7033
MamaniErick <mamanierick64@gmail.com>
2025-06-18 11:26:48 -0300

    Importacion de Componenente[NotFound,jsx][MamaniErick]

--- pv_tp_integrador_grupo16/src/App.jsx
+++ pv_tp_integrador_grupo16/src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound.jsx';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<></>} />
         <Route path="/favoritos" element={<></>} />
         <Route path="/producto/:id" element={<></>} />
         <Route path="/crear" element={<></>} />
