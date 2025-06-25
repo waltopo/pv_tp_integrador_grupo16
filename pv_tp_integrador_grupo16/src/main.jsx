0321c5639bb30d15536207af1f171c647a6c4d52
lucasortiz512 <lucasortiz.lmo@gmail.com>
2025-06-25 19:20:23 -0300

    Comentarios

--- pv_tp_integrador_grupo16/src/main.jsx
+++ pv_tp_integrador_grupo16/src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; // importado de react 
+import ReactDOM from 'react-dom/client'; // importado de la libreria react
 import App from './App.jsx';
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'; // importado de redux para manejar o compartir los archivos slice
 
-import { store } from './redux/store.js';
+import { store } from './redux/store.js'; // importa el store principal
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={store}> {/*permite el acceso al store para todos los componentes*/}
+      <App /> {/*llama a la app principal */}
     </Provider>
   </React.StrictMode>
 );
--- pv_tp_integrador_grupo16/src/redux/store.js
+++ pv_tp_integrador_grupo16/src/redux/store.js
@@ -1,9 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import productsReducer from './productSlice.js';
-import authReducer from './authSlice.js';
-export const store = configureStore({
+import { configureStore } from '@reduxjs/toolkit'; // Libreria de redux para configurar el store
+import productsReducer from './productSlice.js'; // Importa acciones que hacen los objetos 
+import authReducer from './authSlice.js'; // Importa acciones que hacen los objetos
+export const store = configureStore({ 
   reducer: {
-    products: productsReducer,
+    products: productsReducer, // asocia la clave al reducer importado
     auth:authReducer,
   },
-});
+}); // configuracion del store
